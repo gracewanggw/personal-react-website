26248ddaa1be5c44c704e7e52cd3e7a3e4424ef3
gracewanggw <grace.wang.24@dartmouth.edu>
2021-05-10 16:10:39 -0400

    remove firebase

--- website/src/App.js
+++ website/src/App.js
@@ -1,25 +1,14 @@
 import pic from "./PC_Group.jpg";
 import './App.css';
-import Firebase from './firestore/index';
 import Navbar from "./components/Navbar/Navbar";
 import HelloCounter from "./components/HelloCounter.js";
 import FunFacts from "./components/FunFacts.js";
 import Jeopardy from "./components/Jeopardy.js";
-import ToDoBoard from "./components/ToDoBoard";
 
 
 function App() {
-  console.log(Firebase);
-  console.log(Firebase.db);
   const header = ["Hi there!", <br />,"My name is Grace. Welcome to my website!"];
   const about = ["I am a Dartmouth '24 from Houston, Texas.",<br />, "Currently, I am staying in Park City, Utah with a few friends."];
-  // const funFacts = <ul>
-  //   <li>I LOVE food</li>
-  //   <li>I'm currently trying to learn how to play the ukulele</li>
-  //   <li>I had to be sledded down by ski patrol the last time I went skiing</li>
-  // </ul>
-  const funFacts = ["I LOVE food", "I'm currently trying to learn how to play the ukulele",
-   "I had to be sledded down by ski patrol the last time I went skiing"];
 
   const contact = <ul>
     <li><a href="https://www.instagram.com/grace.wangggg/">Instagram</a> </li>
@@ -43,7 +32,6 @@ function App() {
       </body>
       <Jeopardy/>
       <HelloCounter/>
-      <ToDoBoard/>
     </div>
   );
 }
